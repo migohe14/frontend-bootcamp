f0ec17c31ebf5b9ae9526ddfec84feec7376014d
Miguel González <migohe14@gmail.com>
2019-04-16 17:44:31 +0200

    2 step 02

--- step2-02/exercise/src/components/TodoFooter.tsx
+++ step2-02/exercise/src/components/TodoFooter.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Store } from '../store';
-
-// TODO: import DefaultButton, Stack, and Text
+import { DefaultButton, Stack, Text } from 'office-ui-fabric-react';
 
 interface TodoFooterProps {
   clear: () => void;
@@ -11,19 +10,12 @@ interface TodoFooterProps {
 export const TodoFooter = (props: TodoFooterProps) => {
   const itemCount = Object.keys(props.todos).filter(id => !props.todos[id].completed).length;
 
-  // TODO:
-  // 1. replace the <footer> with the Fabric control <Stack>
-  // 2. replace the <span> with the Fabric control <Text>
-  // 3. replace the <button> with Fabric control <DefaultButton>
-
   return (
-    <footer>
-      <span>
-        {itemCount} item{itemCount <= 1 ? '' : 's'} left
-      </span>
-      <button onClick={() => props.clear()} className="button">
-        Clear Completed
-      </button>
-    </footer>
+    <Stack horizontal horizontalAlign="space-between">
+      <Text>
+        {itemCount} item{itemCount === 1 ? '' : 's'} left
+      </Text>
+      <DefaultButton onClick={() => props.clear()}>Clear Completed</DefaultButton>
+    </Stack>
   );
 };
